0454bb2b13511d61c91f5fb55c59070dda4249fa
Jhonatan Pinheiro <48757466+JhonatanPinheiro@users.noreply.github.com>
2021-11-18 11:17:51 -0300

    Update python_text.py

--- python_text.py
+++ python_text.py
@@ -74,3 +74,43 @@
 ##necessariamente precisar de ajuda de outra pessoa para criar uma parte do sistema.
 ##De certa foram é um profissional mais valorizado no mercado, especialmente se for realmente bom em
 ##frontend e backend.
+
+
+
+
+#----------------------------------- 7. Programação Estática x Dinâmica.
+
+##Usamos o termo programação estática ou dinâmica para nos referir do uso de linguagens de
+##programação que não são processados no servidor (estática) ou que são processados no servidor
+##(dinâmica).
+##Como exemplos de linguagens estáticas temos HTML, CSS e JavaScript, etc.
+##Como exemplos de linguagens dinâmicas temos Python, Java, Ruby, PHP, C#, JavaScript, etc.
+
+
+#------------------------------------8. O mínimo que você precisa saber sobre HTML
+
+##HTML é a sigla para Hypertext Markup Language, ou Linguagem de Marcação para Hipertexto.
+##Utilizamos HTML para criar estruturas para páginas web: seções, parágrafos, cabeçalhos, divisões,
+##links e etc.
+##Toda página na internet que você acessa se você clicar com o botão do mouse no navegador e pedir
+##para ver o código fonte será mostrado o conteúdo HTML da página
+
+#Ao trabalhar com HTML nós codificamos estruturas (tags e atributos) para marcar a página de um
+##site, por isso o HTML é chamado de linguagem de marcação. Um arquivo HTML tem extensão ‘.html’.
+
+##De forma geral, uma página web é dividida em 3 área principais: header, body e footer.
+##Uma página web é renderizada pelo navegador de cima para baixo, linha a linha.
+##Header: Usado para tags de informação sobre o
+##site, aplicação de estilos css e etc.
+
+###Body: Conteúdo do site que é apresentado ao cliente.
+###Footer: Geralmente usado para carregamento de
+###scripts Javascript e apresentação de dados de rodapé.
+###OBS: O footer fica dentro da área de conteúdo
+
+
+#Podemos iniciar um servidor web simples com Python utilizando o comando abaixo:
+##python3 -m http.server 8000
+
+
+
